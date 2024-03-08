fde9359d615fa9845d20c785b4b9dbe0d6919e96
Nino Ross <9670771+Chibdibs@users.noreply.github.com>
2024-03-08 18:31:38 +0900

    added the blockchain code and flask methods

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import hashlib
 import time
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 
 
 class Block:
@@ -17,6 +17,8 @@ class Block:
 
 
 class Blockchain:
+    difficulty = 2  # difficulty of the proof of work algorithm
+
     def __init__(self):
         self.unconfirmed_transactions = []
         self.chain = []
@@ -27,11 +29,61 @@ class Blockchain:
         genesis_block.hash = genesis_block.compute_hash()
         self.chain.append(genesis_block)
 
-    # Add more methods here for adding transactions, adding blocks, proof of work, etc.
+    def proof_of_work(self, block):
+        block.nonce = 0
+        computed_hash = block.compute_hash()
+        while not computed_hash.startswith('0' * Blockchain.difficulty):
+            block.nonce += 1
+            computed_hash = block.compute_hash()
+        return computed_hash
+
+    def add_block(self, block, proof):
+        previous_hash = self.last_block.hash
+        if previous_hash != block.previous_hash:
+            return False
+
+        if not self.is_valid_proof(block, proof):
+            return False
+
+        block.hash = proof
+        self.chain.append(block)
+        return True
+
+    def is_valid_proof(self, block, block_hash):
+        return (block_hash.startswith('0' * Blockchain.difficulty) and
+                block_hash == block.compute_hash())
+
+    def add_new_transaction(self, transaction):
+        self.unconfirmed_transactions.append(transaction)
+
+    @property
+    def last_block(self):
+        return self.chain[-1]
 
 
 # Initialize Flask app
 app = Flask(__name__)
+
+blockchain = Blockchain()
+
+@app.route('/chain', methods=['GET'])
+def get_chain():
+    chain_data = []
+    for block in blockchain.chain:
+        chain_data.append(block.__dict__)
+    return jsonify(chain_data, 200
+
+@app.route('/transactions/new', methods=['POST'])
+def new_transaction():
+    values = request.get_json()
+    required_fields = ["sender", "recipient", "amount"]
+
+    if not all(k in values for k in required_fields):
+        return 'Missing values', 400
+
+    index = blockchain.add_new_transaction(values)
+    response = {'message': f'Transaction will be added to Block {index}'}
+    return jsonify(response)
 
 # Add Flask routes for API endpoints
 
